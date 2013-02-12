87c04591fee2ff93061c6aed89f0b47cf4c1d8fb
Mihai Maruseac <mihai@rosedu.org>
2013-02-12 19:23:38 +0200

    Implement simple example of the framework.

--- SimpleEchoExample.hs
+++ SimpleEchoExample.hs
@@ -9,14 +9,30 @@ module Main where
 -- true for the case where «@stderr» is in the ouput part.
 --
 -- Assume that the stdin description is valid: no «@stdout»/«@stderr» on the
--- input part and no «@stdin» on the output part.
+-- input part and no «@stdin» on the output part. Also, no file appears both
+-- in the input and output part.
 
 -- Import IO code from IOManager.
 import IOManager
 
 -- The main function as a pipeline.
-main = readInput >>= return . solve >>= writeOutput
+main :: IO ()
+main = readInput >>= return . uncurry solve >>= writeOutput
 
 -- Solution of the problem. Add here your implementation.
-solve :: Input -> Output
-solve = undefined
+solve :: Input -> Output -> Output
+solve i = convert (map words . lines . getStdIn $ i) i
+
+convert :: [[String]] -> Input -> Output -> Output
+convert [] _ o = o
+convert ([fi, fo]:fs) i o = convert fs i $ link fi fo i o
+convert _ _ _ = error "Invalid input"
+
+link :: String -> String -> Input -> Output -> Output
+link "@stdin" fo i o = write fo (getStdIn i) o
+link fi fo i o = write fo (getInputFile i fi) o
+
+write :: String -> String -> Output -> Output
+write "@stdout" text o = writeStdOut o text
+write "@stderr" text o = writeStdErr o text
+write fi text o = writeOutputFile o fi text
